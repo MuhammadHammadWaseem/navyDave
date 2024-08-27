4d28691ca2fcaa0d2f970d6a004f0b173195ce75
Hammad-waseem-developer <107311660+MuhammadHammadWaseem@users.noreply.github.com>
2024-08-26 18:59:55 -0700

    routwe !

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,8 @@ use App\Http\Controllers\GuestController;
 
 
 // Guest Route
-Route::get('/',[GuestController::class, 'index'])->name('home');
+// Route::get('/',[GuestController::class, 'index'])->name('home');
+Route::get('/',function () {return view('guest.home');})->name('home');
 Route::get('/home',[GuestController::class, 'home'])->name('index');
 Route::get('about',[GuestController::class, 'about'])->name('about');
 Route::get('pricing',[GuestController::class, 'pricing'])->name('pricing');
