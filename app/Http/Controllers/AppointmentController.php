7ae3c921db9f7bb9e6487a3139b5d3a10e354362
Hammad-waseem-developer <nasirahmedwaseemnasir@gmail.com>
2025-01-09 16:28:03 -0800

    Set Issue !!

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\DB;
 use App\Models\GoogleCredential;
 use App\Models\User;
 use Google_Client;
-use App\Jobs\sendUpdateMail;
+use App\Jobs\SendUpdateMail;
 use App\Services\GoogleCalendarService;
 
 class AppointmentController extends Controller
